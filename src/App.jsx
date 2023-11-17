ac84b9d573f1cbe642729b7f90a076922a5c5977
Zohid <777aaauz@gmail.com>
2023-11-17 13:27:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import PostsPage from './pages/PostsPage'
+import PostPage from './pages/PostPage'
+import AddPostPage from './pages/AddPostPage'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import EditPostPage from './pages/EditPostPage'
+import { ToastContainer } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import 'react-toastify/dist/ReactToastify.css'
+import { getMe } from './redux/features/auth/authSlice'
+
+import './App.css'
+
+function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getMe())
+  }, [])
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='posts' element={<PostsPage />} />
+        <Route path=':id' element={<PostPage />} />
+        <Route path=':id/edit' element={<EditPostPage />} />
+        <Route path='new' element={<AddPostPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
+      <ToastContainer position='bottom-right' />
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { checkIsAuth, logout } from '../redux/features/auth/authSlice';
+import {toast} from 'react-toastify'
+
+const Navbar = () => {
+  const isAuth = useSelector(checkIsAuth)
+  console.log(isAuth)
+  const dispatch = useDispatch()
+
+  const activeStyle = {
+    color: 'white'
+  }
+
+  const logoutHandler = () => {
+    dispatch(logout())
+    window.localStorage.removeItem('token')
+    toast('Вы вышли из системы!')
+  }
+
+  return (
+    <div className='flex py-4 justify-between items-center'>
+      <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>
+        E
+      </span>
+      {
+        isAuth && (
+          <ul className="flex gap-8">
+            <li>
+              <NavLink
+                to={"/"}
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyle : undefined}
+              >
+                Главная
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/posts"}
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyle : undefined}
+              >
+                Мои посты
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/new"}
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyle : undefined}
+              >
+                Добавить посит
+              </NavLink>
+            </li>
+          </ul>
+        )
+      }
+      <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2">
+        {
+          isAuth ? (
+            <button onClick={logoutHandler}>Выйти</button>
+          ) : (
+              <Link to={'/login'}>Войти</Link>
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
